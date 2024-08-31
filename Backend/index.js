5ae10e7c63fd0ce8c7d1faea098fc38f151bd517
Anilrajput6441 <anilrajput6441@gmail.com>
2024-08-31 15:47:29 +0530

    multer concepts

--- Backend/index.js
+++ Backend/index.js
@@ -2,6 +2,8 @@ import express from "express";
 import cors from "cors";
 import bodyParser from "body-parser";
 import mysql2 from "mysql2";
+import multer from "multer";
+import fs from "fs";
 
 const app = express();
 app.use(cors());
@@ -15,8 +17,41 @@ const conn=mysql2.createConnection({
   port:3306,
 });
 
+var storage = multer.diskStorage({
+  destination: function (req,file,cb){
+    var dir = "./uploads";
+    if (!fs.existsSync(dir)) {
+      fs.mkdirSync(dir);
+    }
+    cb(null,dir);
+  },
+});
 
+var upload = multer({ storage: storage});
 
+app.post("/fileUpload/img1", (req,res) => {
+  const img1 = req.file.img1;
+  const img2 = req.file.img2;
+  const img3 = req.file.img3;
+  
+  const sql="INSERT INTO blog(img1,img2,img3) VALUES(?,?,?);";
+  conn.query(sql,[img1,img2,img3],(err,data)=>{
+    if(err) console.log(err);
+    else{
+      return res.status(200).send({status: 200});
+    }
+  })
+});
+
+app.get("/fileUpload", (req, res) => {
+  const sql = "SELECT * FROM blogs;";
+  conn.query(sql, (err,data)=>{
+    if(err) console.log(err);
+    else{
+      return res.json(data);
+    }
+  });
+  });
 
 
 app.post("/blog", (req, res) => {
